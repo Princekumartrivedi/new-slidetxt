2c4dbe723571cde4abffcf222b82728d1a900a3a
princekumartrivedi <>
2023-10-03 00:35:20 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        margin: 10,
+        loop: true,
+        //center: true,
+        //mouseDrag: true,
+        //pullDrag: true,
+        //freeDrag: true
+        //stagePadding: 50,
+        //startPosition: 3,
+        //startPosition: 'URLHash'
+        nav: true,
+        //dots: false,
+        dotsEach: true,
+    });
+  });
